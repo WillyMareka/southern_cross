54c708a548eabef71a92d8d8bee7f315f28bc5b3
johnotaalo <john.otaalo@strathmore.edu>
2014-10-03 23:54:09 +0300

    Announcements

---
+++ application/modules/announcements/controllers/announcements.php
@@ -0,0 +1,14 @@
+<?php 
+
+class Announcements extends MX_Controller
+{
+	function __construct()
+    {
+        // Call the Model constructor
+        parent::__construct();
+        $this->load->model('m_announcements');
+    }
+	function index()
+	{
+	}
+}
---
+++ application/modules/announcements/models/m_announcements.php
@@ -0,0 +1,10 @@
+<?php 
+
+class M_Announcements extends MX_Controller
+{
+	function __construct()
+    {
+        // Call the Model constructor
+        parent::__construct();
+    }
+}
