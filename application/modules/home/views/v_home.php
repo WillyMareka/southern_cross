03f526aebca70851f8bfcf435bef7ddc6e9b4e3f
Willy Mareka <marekawilly@gmail.com>
2014-10-04 03:34:23 +0300

    new home page

--- application/modules/home/views/v_home.php
+++ application/modules/home/views/v_home.php
@@ -13,7 +13,9 @@
 
 	</head>
 	<body>
-<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+  <div class="home-body">
+
+    <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
   <div class="container-fluid">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
@@ -23,32 +25,33 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" data-toggle="modal" data-target="#myModal" href="#">SOUTHERN CROSS</a>
+      <a class="navbar-brand" data-toggle="modal" data-target="#myModal" href="#"><img class="home-icon" src="<?php echo base_url(). 'assets/icons/logo.jpg'?>"></a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-		
-        <li><a href="#Home">Home</a></li>
+    
+        <li class="active"><a href="#Home">Home</a></li>
+        <li><a href="#Status">Status</a></li>
         <li><a href="#Contacts">Contacts</a></li>
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown">Applications<span class="caret"></span></a>
           <ul class="dropdown-menu" role="menu">
-            <li><a href="<?php echo base_url()."applications/doctoral"; ?>">Doctoral</a></li>
-            <li><a href="<?php echo base_url()."applications/masters"; ?>">Masters</a></li>
-            <li><a href="<?php echo base_url()."applications/bachelor"; ?>">Bachelor</a></li>
-            <li><a href="<?php echo base_url()."applications/undergraduate"; ?>">Under-Graduate</a></li>
+            <li><a href="#">Doctoral</a></li>
+            <li><a href="#">Masters</a></li>
+            <li><a href="#">Bachelor</a></li>
+            <li><a href="#">Under-Graduate</a></li>
           </ul>
         </li>
       </ul>
       
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="<?php echo base_url()."courses"; ?>">Announcements</a></li>
+        <li><a href="#">Courses</a></li>
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown">E-Learning<span class="caret"></span></a>
           <ul class="dropdown-menu" role="menu">
-            <li><a href="#">Course</a></li>
+            <li><a href="#">Courses</a></li>
             <li><a href="#">Library</a></li>
             <li><a href="#">Exams</a></li>
             <li class="divider"></li>
@@ -62,79 +65,53 @@
 </nav>
 
 <!-- Modal -->
-<div class="modal fade bs-example-modal-lg" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-lg">
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
     <div class="modal-content">
-
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
         <h4 class="modal-title" id="myModalLabel">SOUTHERN CROSS INSTITUTE OF TROPICAL MEDICINE</h4>
       </div>
-
       <div class="modal-body southernimage">
-        
+       
       </div>
-
       <div class="modal-footer">
-        <button type="button" class="btn btn-inverse" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
       </div>
-
     </div>
   </div>
 </div>
 
-<div class="jumbotron home-top" id="Home">
-  <div class="home-top-text"> 
-  <h1>Condusive environment</h1>
-  <h3>Feel confortable while studying</h3>
-  </div>
 
-  <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-  <!-- Indicators -->
-  <ol class="carousel-indicators">
-    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="3"></li>
-  </ol>
-
-  <!-- Wrapper for slides -->
-  <div class="carousel-inner home-slide">
-
-    <div class="item active">
-      <div class="carousel-caption">
-    <h4>"Isn't it a bit unnerving that doctors call what they do "practice"?"</h4>
-    <p>- George Carlin</p>
-  </div>
-  <img src="<?php echo base_url(). 'assets/images/slideimage.jpg'?>" alt="plain_black_image">
-</div>
-
-<div class="item">
-  <div class="carousel-caption">
-    <h4>"Let food be thy medicine and medicine be thy food."</h4>
-    <p>- Hippocrites</p>
-  </div>
-  <img src="<?php echo base_url(). 'assets/images/slideimage.jpg'?>" alt="plain_black_image">
-  
-</div>
-
-<div class="item">
-  <div class="carousel-caption">
-    <h4>"Always laugh when you can, it is cheap medicine."</h4>
-    <p>- George Gordon Byron</p>
-  </div>
-  <img src="<?php echo base_url(). 'assets/images/slideimage.jpg'?>" alt="plain_black_image">
-</div>
-
-<div class="item">
-  <div class="carousel-caption">
-    <h4>"A medicine cat has no time for doubt. Put your energy into today and stop worrying about the past."</h4>
-    <p>- Erin Hunter, Rising Storm</p>
-  </div>
-  <img src="<?php echo base_url(). 'assets/images/slideimage.jpg'?>" alt="plain_black_image">
-</div>
-    
-  </div>
+  <div id="carousel-example-generic" class="carousel slide carou" data-ride="carousel">
+   <!-- Carousel indicators -->
+   <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+      <li data-target="#myCarousel" data-slide-to="3"></li>
+   </ol>   
+   <!-- Carousel items -->
+   <div class="carousel-inner">
+      <div class="item active">
+        <h4>"Isn't it a bit unnerving that doctors call what they do "practice" ? "</h4>
+         
+         <p>- George Carlin</p>
+      </div>
+      <div class="item">
+         <h4>"Let food be thy medicine and medicine be thy food."</h4>
+         <p>- Hippocrites</p>
+      </div>
+      <div class="item">
+         <h4>"Always laugh when you can, it is cheap medicine."</h4>
+         <p>- George Gordon Byron</p>
+      </div>
+      <div class="item">
+         <h4>"A medicine cat has no time for doubt. Put your energy into today and stop worrying about the past."</h4>
+         <p>- Erin Hunter, Rising Storm</p>
+         
+      </div>
+   </div>
 
   <!-- Controls -->
   <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
@@ -143,53 +120,86 @@
   <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
     <span class="glyphicon glyphicon-chevron-right"></span>
   </a>
-</div>
-</div>
+</div> 
 
-<div class="jumbotron home-bottom" id="Contacts">
-  <div class="home-bottom-text">
-      <h1>Contact us to know more</h1>
+
+
+<div class="well well-lg status" id="Status">
+  <div class="row">
+
+  <div class="col-sm-6 col-md-4">
+    <a href="#">
+    <div class="thumbnail status_image">
+      <img src="<?php echo base_url(). 'assets/images/blacknigga.jpg'?>" alt="About">
+      <div class="caption">
+        <h4>About</h4>
+      </div>
+      </div>
+    </a>
+    </div>
+  
+
+  <div class="col-sm-6 col-md-4">
+    <a href="#">
+    <div class="thumbnail status_image">
+      <img src="<?php echo base_url(). 'assets/images/graduate.jpg'?>" alt="Application">
+      <div class="caption">
+        <h4>Application</h4>
+      </div>
+      </div>
+    </a>
+    </div>
+  
+
+  <div class="col-sm-6 col-md-4">
+    <a href="#">
+    <div class="thumbnail status_image">
+      <img src="<?php echo base_url(). 'assets/images/courses.jpg'?>" alt="Courses">
+      <div class="caption">
+        <h4>Courses</h4>
+      </div>
+      </div>
+    </a>
+    </div>
+
   </div>
-  <div id="img_container" onmouseout="hide(this.id);">
-    <img id="large_img" src="" />
   </div>
-  <div class="contact-images">
 
-    <a href="http://www.facebook.com">
-    <button type="button" class="btn btn-default contact-button" data-toggle="tooltip" data-placement="top" title="Facebook">
-     <img id="img1"  class="littleright" width="98" height="100" src="<?php echo base_url(). 'assets/icons/facebook.jpg'?>" onmouseover="show('img_container',this,'large_img');">
-    </button></a>
+ 
 
-    <a href="http://www.gmail.com">
-    <button type="button" class="btn btn-default contact-button" data-toggle="tooltip" data-placement="top" title="Gmail">
-     <img id="img2" class="littleright" width="98" height="100" src="<?php echo base_url(). 'assets/icons/gmail.jpg'?>" onmouseover="show('img_container',this,'large_img');">
-    </button></a>
 
-    <a href="http://www.twitter.com">
-    <button type="button" class="btn btn-default contact-button" data-toggle="tooltip" data-placement="top" title="Twitter">
-     <img id="img3" class="littleright" width="98" height="100" src="<?php echo base_url(). 'assets/icons/twitter.jpg'?>" onmouseover="show('img_container',this,'large_img');">
-    </button></a>
 
-    <a href="http://www.instragram.com">
-    <button type="button" class="btn btn-default contact-button" data-toggle="tooltip" data-placement="top" title="Instagram">
-     <img id="img4" class="littleright" width="98" height="100" src="<?php echo base_url(). 'assets/icons/instagram.jpg'?>" onmouseover="show('img_container',this,'large_img');">
-    </button></a>
+<div class="footer" id="Contacts">
+  <div class="footer_form left">
 
-    <a href="http://www.yahoo.com">
-    <button type="button" class="btn btn-default contact-button" data-toggle="tooltip" data-placement="top" title="Yahoo">
-     <img id="img5" class="littleright" width="98" height="100" src="<?php echo base_url(). 'assets/icons/yahoo.jpg'?>" onmouseover="show('img_container',this,'large_img');">
-    </button></a>
+  <form role="form">
+     <div class="form-group">
+       <label for="exampleInputEmail1">NAME</label>
+       <input type="text" class="form-control" id="InputName" required placeholder="Enter your Name">
+     </div>
+     <div class="form-group">
+       <label for="exampleInputPassword1">EMAIL ADDRESS</label>
+       <input type="email" class="form-control" id="InputEmail" required placeholder="Enter Email">
+     </div>
+     <div class="form-group">
+       <label for="InputMobile">MOBILE NUMBER</label>
+       <input type="text" class="form-control" id="InputMobile" required placeholder="Enter Mobile Number">
+     </div>
+     
+    <button type="submit" class="btn btn-info">Submit</button>
+  </form>
 
   </div>
 
-
+  <div class="footer_contacts right">
+   <a href="http://www.facebook.com"><img class="contact_icons" src="<?php echo base_url(). 'assets/icons/facebook.jpg'?>"><strong> Facebook</strong></a>
+   <a href="http://www.gmail.com"><img class="contact_icons" src="<?php echo base_url(). 'assets/icons/gmail.jpg'?>"><strong> G-Mail</strong></a>
+   <a href="http://www.yahoo.com"><img class="contact_icons" src="<?php echo base_url(). 'assets/icons/yahoo.jpg'?>"><strong> Yahoo</strong></a>
+   <a href="http://www.twitter.com"><img class="contact_icons" src="<?php echo base_url(). 'assets/icons/twitter.jpg'?>"><strong> Twitter</strong></a>
+  </div>
+ </div>
 
 </div>
-
-
-
-
-
 
 		  <script src="<?php echo base_url(). 'assets/flat-ui/js/jquery-1.8.3.min.js'?>"></script>
 	    <script src="<?php echo base_url(). 'assets/flat-ui/js/jquery-ui-1.10.3.custom.min.js'?>"></script>
