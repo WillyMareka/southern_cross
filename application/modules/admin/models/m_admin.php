66b4b1c0b273eb0fd8331b40aff14d2a7c5f3bae
bakasajoshua <baksajoshua09@gmail.com>
2014-10-04 10:20:22 +0300

    Administrator

--- application/modules/admin/models/m_admin.php
+++ application/modules/admin/models/m_admin.php
@@ -13,10 +13,45 @@ class M_admin extends MY_Model {
     {
     	$sql = "SELECT * FROM `applications`";
 
+
     	$apps = $this->db->query($sql);
     	
     	return $apps->result_array();
     }
+
+    function get_courses()
+    {
+        $sql = "SELECT
+                    `course_id`,
+                    `course_name`,
+                    `course_short_code`,
+                    `Description`
+                FROM
+                    `courses`";
+
+        $courses = $this->db->query($sql);
+
+        return $courses->result_array();
+
+    }
+
+    function add_application($path)
+    {
+        $firstname = strtoupper($this->input->post('firstname'));
+        $lastname = strtoupper($this->input->post('lastname'));
+        $others = strtoupper($this->input->post('othername'));
+        $phone = $this->input->post('phonenumber');
+        $gender = $this->input->post('gender');
+        $student_email = $this->input->post('student_email');
+        $parent_phone = $this->input->post('parent_phone');
+        $parent_email= $this->input->post('parent_email');
+        $location = strtoupper($this->input->post('location'));
+        $course = $this->input->post('course');
+
+        $query = "INSERT INTO students VALUES(NULL, '$firstname', '$lastname', '$others', '$phone', '$gender', '$parent_phone', '$student_email', '$parent_email', '$location', '$path', NULL, '$course', 1)";
+        $result = $this->db->query($query);
+    }
+
 
    
 }
