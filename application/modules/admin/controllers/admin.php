4c77f9998907935b8df4bfeeb1acd7a245860c03
bakasajoshua <baksajoshua09@gmail.com>
2014-10-22 18:22:36 +0300

    some changes

--- application/modules/admin/controllers/admin.php
+++ application/modules/admin/controllers/admin.php
@@ -1,14 +1,24 @@
 <?php 
 
-class Admin extends MX_Controller
+class Admin extends MY_Controller
 {
 	function __construct()
     {
         // Call the Model constructor
         parent::__construct();
         $this->load->model('admin_model');
+        $this->load->model('m_admin');
     }
 	function index()
 	{
+		$data['content_view'] = "application_view";
+		$data['application'] = $this->m_admin->applications();
+		$this->load->view('admin_view', $data);
+	}
+
+	function register()
+	{
+		$data['content_view'] = "registercourse";
+		$this->load->view('admin_view', $data);
 	}
 }
