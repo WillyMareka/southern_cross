6e797a9184d792d76c967825c74da05ce07f55c2
johnotaalo <john.otaalo@strathmore.edu>
2014-10-30 13:57:13 +0300

    removed the schema files from dbv

--- dbv/data/schema/courses.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `courses` (
-  `course_id` int(11) NOT NULL AUTO_INCREMENT,
-  `course_name` varchar(250) NOT NULL,
-  `course_short_code` varchar(10) NOT NULL,
-  `Description` text NOT NULL,
-  PRIMARY KEY (`course_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1
