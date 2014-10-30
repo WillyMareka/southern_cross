d65e1d84543af8e5b76c2d39d17d47e7b84f2071
Willy Mareka <marekawilly@gmail.com>
2014-10-30 17:22:22 +0300

    Dbv from richard

---
+++ dbv/data/schema/applicant_personal_info.sql
@@ -0,0 +1,15 @@
+CREATE TABLE `applicant_personal_info` (
+  `applicant_id` int(11) NOT NULL,
+  `f_name` varchar(100) NOT NULL,
+  `s_name` varchar(100) NOT NULL,
+  `l_name` varchar(100) NOT NULL,
+  `dob` varchar(20) NOT NULL,
+  `citizenship` varchar(100) NOT NULL,
+  `birth_country` varchar(100) NOT NULL,
+  `gender` varchar(20) NOT NULL,
+  `marital_status` varchar(100) NOT NULL,
+  `disability` varchar(100) NOT NULL,
+  `disability_info` varchar(250) DEFAULT NULL,
+  `applicant_picture` varchar(250) NOT NULL,
+  PRIMARY KEY (`applicant_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
---
+++ dbv/data/schema/student_groups.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `student_groups` (
+  `id` int(11) NOT NULL,
+  `group_name` varchar(250) NOT NULL,
+  `unit` int(11) NOT NULL,
+  `lecturer_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
