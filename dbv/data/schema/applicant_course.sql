3f3af2c45b2931fe02a2195657c81213bbf613f5
Willy Mareka <marekawilly@gmail.com>
2014-11-04 14:53:26 +0300

    Table modification

--- dbv/data/schema/applicant_course.sql
+++ dbv/data/schema/applicant_course.sql
@@ -2,6 +2,6 @@ CREATE TABLE `applicant_course` (
   `applicant_id` int(11) NOT NULL,
   `course_id` int(11) NOT NULL,
   `intake` int(11) NOT NULL,
-  `date_applied` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_applied` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   KEY `applicant_id` (`applicant_id`,`course_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1
