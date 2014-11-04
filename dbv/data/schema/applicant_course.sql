8e35226205462257b43be223437b32fc87aeeb30
johnotaalo <john.otaalo@strathmore.edu>
2014-11-04 14:42:11 +0300

    applicant course

---
+++ dbv/data/schema/applicant_course.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `applicant_course` (
+  `applicant_id` int(11) NOT NULL,
+  `course_id` int(11) NOT NULL,
+  `intake` int(11) NOT NULL,
+  `date_applied` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  KEY `applicant_id` (`applicant_id`,`course_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
